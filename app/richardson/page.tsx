9ebe8d4ac60e95ac81ebbeb772bcead386db991d
beckfurd <tbrown@beckfurd.com>
2025-12-16 18:42:11 -0700

    Create page.tsx

---
+++ app/richardson/page.tsx
@@ -0,0 +1,8 @@
+export default function RichardsonSearch() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Richardson Inventory Lookup</h1>
+      <p>Search page coming next.</p>
+    </main>
+  );
+}
